9e65c8941c9c0641a81bb598e432a76b9dad961e
Lev Berman <ldmberman@gmail.com>
2015-07-20 14:03:51 +0300

    add test for loading a time.Time field

--- model_loader/model_loader_test.go
+++ model_loader/model_loader_test.go
@@ -43,6 +43,15 @@ var testCases = []modelLoaderTestCase{
 			FieldString: "some string",
 		},
 	},
+	// Loads time.Time field.
+	{
+		args: map[string]interface{}{
+			"FieldDateTime": "2012-02-13T15:40:00",
+		},
+		res: testModel{
+			FieldDateTime: time.Date(2012, time.February, 13, 15, 40, 0, 0, time.UTC),
+		},
+	},
 	// Parses JSON and loads it into object field.
 	{
 		args: map[string]interface{}{
