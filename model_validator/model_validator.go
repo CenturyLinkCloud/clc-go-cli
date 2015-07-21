06468ac5463112cf22035649393432ad88639ab6
Lev Berman <ldmberman@gmail.com>
2015-07-21 14:34:38 +0300

    process and prettyprint govalidator errors

--- model_validator/model_validator.go
+++ model_validator/model_validator.go
@@ -1,14 +1,22 @@
 package model_validator
 
 import (
+	"fmt"
 	"github.com/asaskevich/govalidator"
 	"github.com/centurylinkcloud/clc-go-cli/base"
+	"strings"
 )
 
 func ValidateModel(model interface{}) error {
 	_, err := govalidator.ValidateStruct(model)
 	if err != nil {
-		return err
+		var msg string
+		parts := strings.Split(err.Error(), ";")
+		errors := parts[:len(parts)-1]
+		for _, fieldErr := range errors {
+			msg += fmt.Sprintf("%s\n", fieldErr)
+		}
+		return fmt.Errorf(msg)
 	}
 	if m, ok := model.(base.ValidatableModel); ok {
 		err = m.Validate()
