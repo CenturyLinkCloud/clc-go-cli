02e2eebf07da56ec80b2a13a4c4784a34368883c
Lev Berman <ldmberman@gmail.com>
2015-07-24 17:09:51 +0300

    Add the missing Links field to the ServerRes type.

--- models/server/server_res.go
+++ models/server/server_res.go
@@ -1,7 +1,12 @@
 package server
 
+import (
+	"github.com/centurylinkcloud/clc-go-cli/models"
+)
+
 type ServerRes struct {
 	Server       string
 	IsQueued     bool
 	ErrorMessage string
+	Links        []models.LinkEntity
 }
