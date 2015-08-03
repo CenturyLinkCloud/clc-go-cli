7a75b619a366f87d61a7f6339f2e5c30e9b0a7fe
Lev Berman <ldmberman@gmail.com>
2015-08-03 19:24:25 +0300

    Use a non-nil input model with the login command to forbid unknown arguments

--- commands/login.go
+++ commands/login.go
@@ -4,8 +4,14 @@ type Login struct {
 	CommandBase
 }
 
+type inputStub struct{}
+
 func NewLogin(info CommandExcInfo) *Login {
 	l := Login{}
 	l.ExcInfo = info
 	return &l
+}
+
+func (l *Login) InputModel() interface{} {
+	return &inputStub{}
 }
