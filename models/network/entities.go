7498dc851797a591d7551ca1d47a93495041e6ac
Lev Berman <ldmberman@gmail.com>
2015-08-04 14:39:31 +0300

    Do not forget to define a network.Entity type

---
+++ models/network/entities.go
@@ -0,0 +1,17 @@
+package network
+
+import (
+	"github.com/centurylinkcloud/clc-go-cli/models"
+)
+
+type Entity struct {
+	Id          string
+	Cidr        string
+	Description string
+	Gateway     string
+	Name        string
+	Netmask     string
+	Type        string
+	Vlan        int64
+	Links       []models.LinkEntity
+}
