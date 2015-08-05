e7f9b2b2dc0f0e70bdd1c97859fafcdce2cd1f63
Lev Berman <ldmberman@gmail.com>
2015-08-05 14:44:44 +0300

    Check that ports is not empty as well

--- models/firewall/create.go
+++ models/firewall/create.go
@@ -27,6 +27,10 @@ func (c *CreateReq) Validate() error {
 	if err != nil {
 		return err
 	}
+	err = validatePorts(c.Ports)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -40,6 +44,13 @@ func validateSource(source []string) error {
 func validateDestination(dest []string) error {
 	if len(dest) == 0 {
 		return fmt.Errorf("destination: non-zero value required.")
+	}
+	return nil
+}
+
+func validatePorts(ports []string) error {
+	if len(ports) == 0 {
+		return fmt.Errorf("ports: non-zero value required.")
 	}
 	return nil
 }
