ee2d48c1aa78d1ef9aa227d03629298cf4e87e44
Lev Berman <ldmberman@gmail.com>
2015-08-05 12:25:57 +0300

    Move actions and triggers validation logic to the separate functions

--- models/alert/create.go
+++ models/alert/create.go
@@ -11,9 +11,26 @@ type CreateReq struct {
 }
 
 func (c *CreateReq) Validate() error {
-	if len(c.Actions) == 0 {
+	err := validateActions(c.Actions)
+	if err != nil {
+		return err
+	}
+	err = validateTriggers(c.Triggers)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateActions(actions []Action) error {
+	if len(actions) == 0 {
 		return fmt.Errorf("Actions: non-zero value required.")
-	} else if len(c.Triggers) == 0 {
+	}
+	return nil
+}
+
+func validateTriggers(triggers []Trigger) error {
+	if len(triggers) == 0 {
 		return fmt.Errorf("Triggers: non-zero value required.")
 	}
 	return nil
