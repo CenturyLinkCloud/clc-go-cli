3cfbdaa42a255861e6588b1a9510baf4e68f04af
Lev Berman <ldmberman@gmail.com>
2015-08-06 14:43:17 +0300

    Introduce GetResources that returns a text with all available resources listed

--- command_loader/command_loader.go
+++ command_loader/command_loader.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	cli "github.com/centurylinkcloud/clc-go-cli"
 	"github.com/centurylinkcloud/clc-go-cli/base"
+	"strings"
 )
 
 func LoadCommand(resource, command string) (base.Command, error) {
@@ -25,4 +26,16 @@ func LoadCommand(resource, command string) (base.Command, error) {
 		return nil, fmt.Errorf("Command should be specified. Use 'clc %s --help' to list all avaliable commands.", resource)
 	}
 	return nil, fmt.Errorf("Command %s %s not found. Use 'clc %s --help' to list all avaliable commands.", resource, command)
+}
+
+func GetResources() string {
+	resources := []string{}
+	m := map[string]bool{}
+	for _, cmd := range cli.AllCommands {
+		m[cmd.Resource()] = true
+	}
+	for k, _ := range m {
+		resources = append(resources, k)
+	}
+	return strings.Join(resources, "\n")
 }
