f26f4b9ae54cc75b4bf0cfde0a72d4e326578d23
Lev Berman <ldmberman@gmail.com>
2015-08-12 14:01:40 +0300

    Include the options description in the command help

--- help/help.go
+++ help/help.go
@@ -21,7 +21,49 @@ Parameters:
 {{range .Arguments}}
 	{{.Name}}
 {{range .Description}} {{ printf "\t\t" }} {{ . }} {{ printf "\n" }} {{end}}
-{{end}}`
+{{end}}
+OPTIONS:
+
+	--help
+			Shows general help or help for the given resource or command.
+	--user
+			Specifies a user name for the account.
+	--password
+			Specifies the password for the given user.
+	--profile
+			Specifies a profile to use (one from the config file).
+	--format
+			Specifies the output format - either 'json', 'text' or 'table'.
+			Defaults to 'json'.
+	--generate-cli-skeleton
+			If specified, the command is not actually executed. Instead, JSON with all of
+			its arguments is returned. All the arguments and options specified along with
+			this option are included in the output.
+	--from-file
+			Specifies a JSON file to load command arguments and options from. No other
+			arguments or options can be specified.
+	--query
+			Restricts the fields in the result to only those specified with this option.
+
+			Multiple fields can be separated by comma. Nested fields can be queried using dot.
+			Multiple nested fields are also separated by comma and must be enclosed in curly
+			braces on the deepest level of nesting. Note, that a shell may treat curly braces
+			in a special way so put the whole query in quotes to avoid errors.
+			Aliases may be set for the nested fields using semicolons inside the braces.
+
+			An example:
+				clc server list --query "details.IP-addresses.{I:internal,P:public}"
+	--filter
+			Filters out the returned entities that do not match the given conditions. Multiple
+			conditions are separated via comma. Each condition consists of a field, an
+			operation and a value. Supported operations are:
+
+			=	equals, applicable to strings, numbers and booleans
+			^= (starts with), $= (ends with), ~= (contains)	these three are only for strings
+			<,<=,>,>= 	comparison operators, can be used with numbers and strings
+	--trace
+			If specified, prints out all the HTTP request/response data.
+`
 
 func ForCommand(cmd Command) string {
 	tmpl, err := template.New("command help").Parse(commandHelpTemplate)
