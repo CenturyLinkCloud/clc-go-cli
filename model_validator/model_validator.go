43e4b0a279a88bb3691c28847d90dc55cbd140f6
Lev Berman <ldmberman@gmail.com>
2015-08-20 17:37:20 +0300

    Implement the validation of enumerables
    This commit also introduces FieldOptions - a function that returns all of the options for the given enumerable field

--- model_validator/model_validator.go
+++ model_validator/model_validator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/asaskevich/govalidator"
 	"github.com/centurylinkcloud/clc-go-cli/base"
+	"reflect"
 	"strings"
 )
 
@@ -14,8 +15,72 @@ func ValidateModel(model interface{}) error {
 		errors := parts[:len(parts)-1]
 		return fmt.Errorf(strings.Join(errors, "\n"))
 	}
+	err = validateEnums(model)
+	if err != nil {
+		return err
+	}
 	if m, ok := model.(base.ValidatableModel); ok {
 		err = m.Validate()
 	}
 	return err
+}
+
+func FieldOptions(model interface{}, name string) ([]string, bool) {
+	meta := reflect.ValueOf(model)
+	if meta.Kind() == reflect.Ptr {
+		meta = meta.Elem()
+	}
+	v, typ := meta, meta.Type()
+
+	ft, exists := typ.FieldByName(name)
+	if !exists {
+		return nil, false
+	}
+	fv := v.FieldByName(name)
+	if fv.Kind() != reflect.String {
+		return nil, false
+	}
+
+	tag := ft.Tag.Get("oneOf")
+	if tag == "" {
+		return nil, false
+	}
+
+	return strings.Split(tag, ","), true
+}
+
+func validateEnums(model interface{}) error {
+	var v reflect.Value
+	var typ reflect.Type
+
+	meta := reflect.ValueOf(model)
+	if meta.Kind() == reflect.Ptr {
+		meta = meta.Elem()
+	}
+	if meta.Kind() != reflect.Struct {
+		panic("A struct or a pointer to a struct is expected.")
+	}
+	v, typ = meta, meta.Type()
+
+	numFields := typ.NumField()
+	for i := 0; i < numFields; i++ {
+		name := typ.FieldByIndex([]int{i}).Name
+		opts, exist := FieldOptions(model, name)
+		if exist {
+			field := v.FieldByIndex([]int{i})
+			if !contains(opts, field.String()) {
+				return fmt.Errorf("%s value must be one of %s.", name, strings.Join(opts, ", "))
+			}
+		}
+	}
+	return nil
+}
+
+func contains(strs []string, s string) bool {
+	for _, el := range strs {
+		if s == el {
+			return true
+		}
+	}
+	return false
 }
