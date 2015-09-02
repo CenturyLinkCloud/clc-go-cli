7678fa51bfb7fdf3d3b8f1d32a9e41c96228ba6d
Lev Berman <ldmberman@gmail.com>
2015-09-02 16:16:53 +0300

    Do not forget to introduce server.List

---
+++ models/server/list.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"fmt"
+	"github.com/centurylinkcloud/clc-go-cli/base"
+)
+
+type List struct {
+	DataCenter string
+	All        base.NilField
+}
+
+func (l *List) Validate() error {
+	if !l.All.Set && l.DataCenter == "" {
+		return fmt.Errorf("DataCenter: non zero value required")
+	}
+	return nil
+}
