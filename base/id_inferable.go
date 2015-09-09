4a348d466b954cf028e8a23735e702668c7cc5ff
Lev Berman <ldmberman@gmail.com>
2015-09-09 12:20:34 +0300

    Document the IDInferable interface and its methods

--- base/id_inferable.go
+++ base/id_inferable.go
@@ -1,7 +1,12 @@
 package base
 
-// IDInferable represents models that have IDs that can be infered by names.
+// IDInferable represents models that have IDs that can be infered from names
+// through communicating the server.
 type IDInferable interface {
+	// After InferID has been completed, all the IDs possible have to have
+	// been infered from names.
 	InferID(cn Connection) error
+	// GetNames exists for the purpose of getting the list of names of entities.
+	// The name argument is a field that points to an entity name.
 	GetNames(cn Connection, name string) ([]string, error)
 }
