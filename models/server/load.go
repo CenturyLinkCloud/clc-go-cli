CenturyLinkCloud/clc-go-cli

commit ecffe84de67bf2b5b1cf46f8076ef8240255d7e9

Move the core logic of server/group list to group/server packages
The idea is we need this functionality inside the group/server packages as well that must not depend on the commands. Also, GetLink is moved to the models package in this commit and does not panic anymore because it depends on data from outside.

models/server/load.go
@@ -1,0 +1,93 @@
+package server
+
+import (
+	"fmt"
+	"github.com/centurylinkcloud/clc-go-cli/base"
+	"github.com/centurylinkcloud/clc-go-cli/models"
+	"github.com/centurylinkcloud/clc-go-cli/models/group"
+	"time"
+)
+
+var (
+	ListTimeout = 200
+)
+
+func Load(cn base.Connection, dataCenter string) ([]GetRes, error) {
+	groups, err := group.Load(cn, dataCenter)
+	if err != nil {
+		return nil, err
+	}
+
+	links := []models.LinkEntity{}
+	for _, g := range groups {
+		err := extractServers(g, &links)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	servers := make([]GetRes, len(links))
+	if links == nil {
+		return servers, nil
+	}
+	done := make(chan error)
+	for i, link := range links {
+		go loadServer(link, servers, i, done, cn)
+	}
+
+	serversLoaded := 0
+	for {
+		select {
+		case err := <-done:
+			if err != nil {
+				return nil, err
+			}
+			serversLoaded += 1
+			if serversLoaded == len(servers) {
+				return servers, nil
+			}
+		case <-time.After(time.Second * time.Duration(ListTimeout)):
+			return nil, fmt.Errorf("Request timeout error")
+		}
+	}
+}
+
+func extractServers(g group.Entity, servers *[]models.LinkEntity) error {
+	if g.ServersCount == 0 {
+		return nil
+	}
+
+	for _, link := range g.Links {
+		if link.Rel == "server" {
+			*servers = append(*servers, link)
+		}
+	}
+	if len(g.Groups) != 0 {
+		for _, gnested := range g.Groups {
+			err := extractServers(gnested, servers)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func loadServer(link models.LinkEntity, servers []GetRes, index int, done chan<- error, cn base.Connection) {
+	href, err := models.GetLink([]models.LinkEntity{link}, "server")
+	if err != nil {
+		done <- err
+		return
+	}
+
+	serverURL := fmt.Sprintf("%s%s", base.URL, href)
+	d := GetRes{}
+	err = cn.ExecuteRequest("GET", serverURL, nil, &d)
+	if err != nil {
+		done <- err
+		return
+	}
+
+	servers[index] = d
+	done <- nil
+}

models/group/load.go
@@ -1,0 +1,86 @@
+package group
+
+import (
+	"fmt"
+	"github.com/centurylinkcloud/clc-go-cli/base"
+	"github.com/centurylinkcloud/clc-go-cli/models"
+	"github.com/centurylinkcloud/clc-go-cli/models/datacenter"
+	"time"
+)
+
+var (
+	ListTimeout = 200
+)
+
+func Load(cn base.Connection, dataCenter string) ([]Entity, error) {
+	datacenters := []datacenter.GetRes{}
+
+	if dataCenter == "all" {
+		dcURL := fmt.Sprintf("%s/v2/datacenters/{accountAlias}", base.URL)
+		err := cn.ExecuteRequest("GET", dcURL, nil, &datacenters)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		dc := datacenter.GetRes{}
+		dcURL := fmt.Sprintf("%s/v2/datacenters/{accountAlias}/%s?groupLinks=true", base.URL, dataCenter)
+		err := cn.ExecuteRequest("GET", dcURL, nil, &dc)
+		if err != nil {
+			return nil, err
+		}
+		datacenters = append(datacenters, dc)
+	}
+
+	done := make(chan error)
+	groups := make([]Entity, len(datacenters))
+	for i, ref := range datacenters {
+		go loadGroups(ref, groups, i, cn, done)
+	}
+
+	received := 0
+	for {
+		select {
+		case err := <-done:
+			if err != nil {
+				return nil, err
+			}
+			received += 1
+			if received == len(datacenters) {
+				return groups, nil
+			}
+		case <-time.After(time.Second * time.Duration(ListTimeout)):
+			return nil, fmt.Errorf("Request timeout error.")
+		}
+	}
+}
+
+func loadGroups(ref datacenter.GetRes, groups []Entity, dcnumber int, cn base.Connection, done chan<- error) {
+	// Get detailed DC info.
+	link, err := models.GetLink(ref.Links, "self")
+	if err != nil {
+		done <- err
+		return
+	}
+	d := datacenter.GetRes{}
+	dcURL := fmt.Sprintf("%s%s?groupLinks=true", base.URL, link)
+	err = cn.ExecuteRequest("GET", dcURL, nil, &d)
+	if err != nil {
+		done <- err
+		return
+	}
+	// Get the root group of the given DC.
+	link, err = models.GetLink(d.Links, "group")
+	if err != nil {
+		done <- err
+		return
+	}
+	g := Entity{}
+	gURL := fmt.Sprintf("%s%s", base.URL, link)
+	err = cn.ExecuteRequest("GET", gURL, nil, &g)
+	if err != nil {
+		done <- err
+		return
+	}
+	groups[dcnumber] = g
+	done <- nil
+}

commands/group_list.go
@@ -1,17 +1,8 @@
 package commands
 
 import (
-	"fmt"
 	"github.com/centurylinkcloud/clc-go-cli/base"
-	"github.com/centurylinkcloud/clc-go-cli/models"
-	"github.com/centurylinkcloud/clc-go-cli/models/datacenter"
 	"github.com/centurylinkcloud/clc-go-cli/models/group"
-	"time"
-)
-
-const (
-	BaseURL          = "https://api.ctl.io"
-	GroupListTimeout = 200
 )
 
 type GroupList struct {
@@ -35,81 +26,9 @@ func (g *GroupList) Execute(cn base.Connection) error {
 		code = "all"
 	}
 
-	g.Output, err = GetGroups(cn, code)
+	g.Output, err = group.Load(cn, code)
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func GetGroups(cn base.Connection, code string) ([]group.Entity, error) {
-	datacenters := []datacenter.GetRes{}
-
-	if code == "all" {
-		dcURL := fmt.Sprintf("%s/v2/datacenters/{accountAlias}", BaseURL)
-		err := cn.ExecuteRequest("GET", dcURL, nil, &datacenters)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		dc := datacenter.GetRes{}
-		dcURL := fmt.Sprintf("%s/v2/datacenters/{accountAlias}/%s?groupLinks=true", BaseURL, code)
-		err := cn.ExecuteRequest("GET", dcURL, nil, &dc)
-		if err != nil {
-			return nil, err
-		}
-		datacenters = append(datacenters, dc)
-	}
-
-	done := make(chan error)
-	groups := make([]group.Entity, len(datacenters))
-	for i, ref := range datacenters {
-		go loadGroups(ref, groups, i, cn, done)
-	}
-
-	received := 0
-	for {
-		select {
-		case err := <-done:
-			if err != nil {
-				return nil, err
-			}
-			received += 1
-			if received == len(datacenters) {
-				return groups, nil
-			}
-		case <-time.After(time.Second * GroupListTimeout):
-			return nil, fmt.Errorf("Request timeout error.")
-		}
-	}
-}
-
-func GetLink(links []models.LinkEntity, resource string) string {
-	for _, link := range links {
-		if link.Rel == resource {
-			return link.Href
-		}
-	}
-	panic(fmt.Sprintf("No %s link found", resource))
-}
-
-func loadGroups(ref datacenter.GetRes, groups []group.Entity, dcnumber int, cn base.Connection, done chan<- error) {
-	// Get detailed DC info.
-	d := datacenter.GetRes{}
-	dcURL := fmt.Sprintf("%s%s?groupLinks=true", BaseURL, GetLink(ref.Links, "self"))
-	err := cn.ExecuteRequest("GET", dcURL, nil, &d)
-	if err != nil {
-		done <- err
-		return
-	}
-	// Get the root group of the given DC.
-	g := group.Entity{}
-	gURL := fmt.Sprintf("%s%s", BaseURL, GetLink(d.Links, "group"))
-	err = cn.ExecuteRequest("GET", gURL, nil, &g)
-	if err != nil {
-		done <- err
-		return
-	}
-	groups[dcnumber] = g
-	done <- nil
 }

commands/server_list.go
@@ -1,16 +1,8 @@
 package commands
 
 import (
-	"fmt"
 	"github.com/centurylinkcloud/clc-go-cli/base"
-	"github.com/centurylinkcloud/clc-go-cli/models"
-	"github.com/centurylinkcloud/clc-go-cli/models/group"
 	"github.com/centurylinkcloud/clc-go-cli/models/server"
-	"time"
-)
-
-const (
-	ServerListTimeout = 200
 )
 
 type ServerList struct {
@@ -25,8 +17,8 @@ func NewServerList(info CommandExcInfo) *ServerList {
 }
 
 func (sl *ServerList) Execute(cn base.Connection) error {
-	var links []models.LinkEntity
 	var code string
+	var err error
 	input := sl.Input.(*server.List)
 
 	code = input.DataCenter
@@ -34,73 +26,9 @@ func (sl *ServerList) Execute(cn base.Connection) error {
 		code = "all"
 	}
 
-	groups, err := GetGroups(cn, code)
+	sl.Output, err = server.Load(cn, code)
 	if err != nil {
 		return err
 	}
-	for _, g := range groups {
-		err := extractServers(g, &links)
-		if err != nil {
-			return err
-		}
-	}
-
-	servers := make([]server.GetRes, len(links))
-	if links == nil {
-		sl.Output = servers
-		return nil
-	}
-	done := make(chan error)
-	for i, link := range links {
-		go loadServer(link, servers, i, done, cn)
-	}
-
-	serversLoaded := 0
-	for {
-		select {
-		case err := <-done:
-			if err != nil {
-				return err
-			}
-			serversLoaded += 1
-			if serversLoaded == len(servers) {
-				sl.Output = servers
-				return nil
-			}
-		case <-time.After(time.Second * ServerListTimeout):
-			return fmt.Errorf("Request timeout error")
-		}
-	}
-}
-
-func extractServers(g group.Entity, servers *[]models.LinkEntity) error {
-	if g.ServersCount == 0 {
-		return nil
-	}
-	for _, link := range g.Links {
-		if link.Rel == "server" {
-			*servers = append(*servers, link)
-		}
-	}
-	if len(g.Groups) != 0 {
-		for _, gnested := range g.Groups {
-			err := extractServers(gnested, servers)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func loadServer(link models.LinkEntity, servers []server.GetRes, index int, done chan<- error, cn base.Connection) {
-	serverURL := fmt.Sprintf("%s%s", BaseURL, GetLink([]models.LinkEntity{link}, "server"))
-	d := server.GetRes{}
-	err := cn.ExecuteRequest("GET", serverURL, nil, &d)
-	if err != nil {
-		done <- err
-		return
-	}
-	servers[index] = d
-	done <- nil
+	return err
 }

models/link.go
@@ -1,9 +1,22 @@
 package models
 
+import (
+	"fmt"
+)
+
 type LinkEntity struct {
 	Rel   string
 	Href  string
 	Id    string
 	Name  string
 	Verbs []string
+}
+
+func GetLink(links []LinkEntity, resource string) (string, error) {
+	for _, link := range links {
+		if link.Rel == resource {
+			return link.Href, nil
+		}
+	}
+	return "", fmt.Errorf("No %s link found", resource)
 }

commit 8fd86e32bb38f4be0c5056772afd18389f75fba9

Check if servers exist at all before collecting them
This was broken in ecffe84de67bf2b5b1cf46f8076ef8240255d7e9

models/server/load.go
@@ -27,7 +27,7 @@ func Load(cn base.Connection, dataCenter string) ([]GetRes, error) {
 	}
 
 	servers := make([]GetRes, len(links))
-	if links == nil {
+	if len(links) == 0 {
 		return servers, nil
 	}
 	done := make(chan error)