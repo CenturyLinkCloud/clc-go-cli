bbee003d9adacc0b106274605c8f9a82691a30aa
Lev Berman <ldmberman@gmail.com>
2015-09-18 17:34:15 +0300

    Override base.URL too

--- proxy/utils.go
+++ proxy/utils.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"github.com/centurylinkcloud/clc-go-cli/base"
 	"github.com/centurylinkcloud/clc-go-cli/connection"
 	"net/http"
 	"net/http/httptest"
@@ -9,16 +10,19 @@ import (
 var (
 	server            = &httptest.Server{}
 	realConnectionURL = connection.BaseUrl
+	realBaseURL       = base.URL
 )
 
 func acquire() *http.ServeMux {
 	mux := http.NewServeMux()
 	server = httptest.NewServer(mux)
 	connection.BaseUrl = server.URL + "/"
+	base.URL = server.URL + "/"
 	return mux
 }
 
 func release() {
 	server.Close()
 	connection.BaseUrl = realConnectionURL
+	base.URL = realBaseURL
 }
