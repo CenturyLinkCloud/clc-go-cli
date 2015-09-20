6c2e066249ebab90e5e0de8949cc8ffce0f39ce0
Lev Berman <ldmberman@gmail.com>
2015-09-20 22:51:51 +0300

    Do not limit the number of possible invokations of Server
    Because it is actually useless and even harmful. The original intent was to make the function thread-safe but we'd better avoid allowing such tests to run in parallel for now.

--- proxy/server.go
+++ proxy/server.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/centurylinkcloud/clc-go-cli/models/authentication"
 	"net/http"
-	"sync"
 )
 
 var (
@@ -12,9 +11,6 @@ var (
 		AccountAlias: "ALIAS",
 		BearerToken:  "token",
 	}
-
-	once        sync.Once
-	andOnlyOnce sync.Once
 )
 
 type Endpoint struct {
@@ -23,24 +19,21 @@ type Endpoint struct {
 }
 
 // Server mocks HTTP requests for the registered endpoints. It is a caller's
-// responsibility to clean up the resources by calling CloseServer. For the purpose
-// of thread-safety only the first invokations of Server and CloseServer work.
+// responsibility to clean up the resources by calling CloseServer.
 func Server(endpoints []Endpoint) {
-	once.Do(func() {
-		mux := acquire()
-		for _, e := range endpoints {
-			mux.HandleFunc(e.URL, func(w http.ResponseWriter, r *http.Request) {
-				js, err := json.Marshal(e.Response)
-				if err != nil {
-					panic(err)
-				}
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(js)
-			})
-		}
-	})
+	mux := acquire()
+	for _, e := range endpoints {
+		mux.HandleFunc(e.URL, func(w http.ResponseWriter, r *http.Request) {
+			js, err := json.Marshal(e.Response)
+			if err != nil {
+				panic(err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+		})
+	}
 }
 
 func CloseServer() {
-	andOnlyOnce.Do(release)
+	release()
 }
