44641efe2960a489defca17fa471ac1f4b0acaf8
Lev Berman <ldmberman@gmail.com>
2015-09-30 15:36:34 +0300

    Test setting/getting the last result

---
+++ state/last_result_test.go
@@ -0,0 +1,54 @@
+package state_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/centurylinkcloud/clc-go-cli/proxy"
+	"github.com/centurylinkcloud/clc-go-cli/state"
+)
+
+type Result struct {
+	Status string
+}
+
+func TestLastResult(t *testing.T) {
+	proxy.Config()
+	defer proxy.Config()
+
+	r := Result{Status: "http://.../status"}
+	err := state.SaveLastResult(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes, err := state.LoadLastResult()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var got Result
+	err = json.Unmarshal(bytes, &got)
+	if err != nil {
+		t.Errorf("Result unmarshalling failed: %s", err.Error())
+	}
+	if got != r {
+		t.Errorf("Invalid result\nExpected %v\nGot %v", r, got)
+	}
+
+	// Repeat to check that we only store one result at a time.
+	r.Status = "http://"
+	err = state.SaveLastResult(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes, err = state.LoadLastResult()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = json.Unmarshal(bytes, &got)
+	if err != nil {
+		t.Errorf("Result unmarshalling failed: %s", err.Error())
+	}
+	if got != r {
+		t.Errorf("Invalid result\nExpected %v\nGot %v", r, got)
+	}
+}
