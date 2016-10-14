732a29d8d0333b26f878ffd86a6601ab2d07b8ce
Richard Case <richard.case@outlook.com>
2016-10-14 10:38:21 +0100

    API file gerenation now has a separate command

--- integration_tests/integration_test.go
+++ integration_tests/integration_test.go
@@ -10,34 +10,15 @@ import (
 
 var (
 	apiPath  = flag.String("api-path", "", "The path to the API file")
-	generate = flag.Bool("generate", false, "Flag to indicate we should generate the API file")
 	clcTrace = flag.Bool("clc-trace", false, "Output trace statements for calls to CLC")
 )
 
 func TestMain(m *testing.M) {
 	flag.Parse()
 
-	if *generate {
-		//Generate the API file
-		logger := NewLogger()
-		parser := NewParser(logger)
-		apiDef, err := parser.ParseApi()
-		if err != nil {
-			logger.Logf("Error while parsing API definition: %v", err)
-			os.Exit(-1)
-		}
-		err = StoreApi(apiDef, *apiPath)
-		if err != nil {
-			logger.Logf("Error while storing API definition: %v", err)
-			os.Exit(-2)
-		}
-		os.Exit(0)
-	}
-
 	result := m.Run()
 
 	os.Exit(result)
-
 }
 
 func TestCommands(t *testing.T) {
